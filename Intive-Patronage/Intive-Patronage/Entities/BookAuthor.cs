8aa98639ea3b2aadcebe00740114cd5a566cbdfb
Michał Szczepaniak <52294433+Kepeczsz@users.noreply.github.com>
2022-12-30 13:10:16 +0100

    Update BookAuthor.cs

--- Intive-Patronage/Intive-Patronage/Entities/BookAuthor.cs
+++ Intive-Patronage/Intive-Patronage/Entities/BookAuthor.cs
@@ -2,19 +2,10 @@
 {
     public class BookAuthor
     {
-        // klucz obcy właściwości nawigacyjnej Employee
+        
         public int AuthorId { get; set; }
-
-        // Właściwość nawigacyjna encji Employee
         public Author Author { get; set; }
-
-
-        // klucz obcy właściwości nawigacyjnej Project
         public int BookId { get; set; }
-
-        // Właściwość nawigacyjna encji Project
         public Book Book { get; set; }
-
-       
     }
 }
