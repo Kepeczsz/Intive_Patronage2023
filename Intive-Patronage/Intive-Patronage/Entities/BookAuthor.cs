e8c466a53295228d96f050f0cdddac11609dc3e5
Kepeczsz <miher13@wp.pl>
2023-01-07 20:45:56 +0100

    resolve merge problems

--- Intive-Patronage/Intive-Patronage/Entities/BookAuthor.cs
+++ Intive-Patronage/Intive-Patronage/Entities/BookAuthor.cs
@@ -1,6 +1,5 @@
 ﻿namespace Intive_Patronage.Entities
 {
-<<<<<<< HEAD
    public class BookAuthor
    {
       public int BookAuthorId { get; set; }
@@ -9,13 +8,4 @@
       public int AuthorId { get; set; }
       public Author Author { get; set; }
    }
-=======
-    public class BookAuthor
-    {
-        public int AuthorId { get; set; }
-        public Author Author { get; set; }
-        public int BookId { get; set; }
-        public Book Book { get; set; }
-    }
->>>>>>> a475901470877edfa4b24aa3277ce4c38837b2d5
 }
